be4bc8a614029bab95402592df31e0e8ea68cfe1
ttxlnguyen <ttxl.nguyen@gmail.com>
2024-08-14 22:08:12 -0400

    TDD

--- src/main/java/HurtLockerLogic.java
+++
@@ -1,9 +0,0 @@
-import java.io.File;
-import java.io.IOException;
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-public class HurtLockerLogic {
-
-}
--- src/test/java/HurtLockerTest.java
+++ src/test/java/HurtLockerTest.java
@@ -1,8 +1,43 @@
-import static org.junit.Assert.*;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
+import java.util.*;
 
 public class HurtLockerTest {
 
+    @Test
+    public void testGetList() {
+        int expected = 5;
+        String s = "xxxxx";
+        List<String> list = Main.returnList("x", s);
+        int actual = list.size();
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void formatting() {
+        String expected = "    Milk";
+        String actual = Main.formatting("Milk");
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void formatting2() {
+        String expected = "22222222";
+        String actual = Main.formatting("22222222");
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testProcess() {
+        String expected = ""+
+                "name:    Milk       seen: 3 times\n" +
+                "=============       =============\n" +
+                "Price:   4.99       seen: 3 times\n" +
+                "-------------       -------------\n\n";
+        ArrayList<String> arr = new ArrayList<>();
+        arr.add("4.99");
+        arr.add("4.99");
+        arr.add("4.99");
+        String actual = Main.logic(arr, "Milk");
+        Assert.assertEquals(expected, actual);
+    }
 }
